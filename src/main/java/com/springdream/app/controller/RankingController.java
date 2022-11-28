79d15c102f31605dcae26e8a7556dca02803ee78
leejunsang <miraclelee0613@gmail.com>
2022-11-28 15:45:55 +0900

    rename MemberPointMapper.xml to PointMapper.xml, modify point-relation files

--- src/main/java/com/springdream/app/controller/RankingController.java
+++ src/main/java/com/springdream/app/controller/RankingController.java
@@ -1,7 +1,9 @@
 package com.springdream.app.controller;
 
+import com.springdream.app.service.RankingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,4 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/ranking")
 public class RankingController {
+    private final RankingService rankingService;
+
+    //    랭킹 목록
+    @GetMapping("/list")
+    public void list(Long memberRank, Model model) {
+
+    }
 }
--- src/main/java/com/springdream/app/repository/PointDAO.java
+++ src/main/java/com/springdream/app/repository/PointDAO.java
@@ -1,18 +1,62 @@
 package com.springdream.app.repository;
 
 
+import com.springdream.app.domain.Criteria;
+import com.springdream.app.domain.PointVO;
+import com.springdream.app.mapper.PointMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class PointDAO {
-//    private final PointMapper pointMapper;
+    private final PointMapper pointMapper;
 
-//    포인트 조회
+    //    포인트 조회
+    public PointVO findById(Long memberNumber){
+        return pointMapper.select(memberNumber);
+    }
 
-//    누적 포인트
+    public List<PointVO> findAll(Criteria criteria){
+        return pointMapper.rankList(criteria);
+    }
 
-//    채택 포인트 전송
+    //    누적 포인트
+    public PointVO totalPointById(Long memberNumber){
+        return pointMapper.totalPoint(memberNumber);
+    }
 
+    //    채택 포인트 전송 (1 to 2)
+    public PointVO sendPoint(Long memberNumber1, Long memberNumber2){
+        
+    }
+
+
+//    private final BoardMapper boardMapper;
+//    //    추가
+//    public void save(BoardDTO boardDTO){
+//        boardMapper.insert(boardDTO);
+//    }
+//    //    수정
+//    public void setBoardVO(BoardDTO boardDTO){
+//        boardMapper.update(boardDTO);
+//    }
+//    //    삭제
+//    public void remove(Long boardNumber){
+//        boardMapper.delete(boardNumber);
+//    }
+//    //    조회
+//    public BoardVO findById(Long boardNumber){
+//        return boardMapper.select(boardNumber);
+//    }
+//    //    전체 조회
+//    public List<BoardVO> findAll(Criteria criteria){
+//        return boardMapper.selectAll(criteria);
+//    }
+//    //    전체 개수
+//    public int findCountAll(){
+//        return boardMapper.getTotal();
+//    }
 }

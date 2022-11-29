613f9966dc9db710da974cb758ce24ba8778dcc9
kmg2331 <115636546+kmg2331@users.noreply.github.com>
2022-11-30 01:24:01 +0900

    Update BoardMapper.java

--- src/main/java/com/springdream/app/mapper/BoardMapper.java
+++ src/main/java/com/springdream/app/mapper/BoardMapper.java
@@ -11,20 +11,7 @@ import java.util.List;
 
 @Mapper
 public interface BoardMapper {
-<<<<<<< HEAD
 
-    public void insert(BoardVO boardVO);
-
-    public void update(BoardVO boardVO);
-
-    public void delete(long boardNumber);
-
-    public BoardVO select(Long boardNumber);
-
-    public List<BoardVO> selectAll();
-
-    public int getTotal();
-=======
 //    추가
     public void insert(BoardVO boardVO);
 //    수정
@@ -35,5 +22,5 @@ public interface BoardMapper {
     public BoardDTO select(Long boardNumber);
 //    전체 조회
     public List<BoardDTO> selectAll();
->>>>>>> 96b6b6afb51683d395c065a2d524b8e3d8cb1165
+    
 }

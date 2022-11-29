3475049e92ed9c95af62d34b56bf623e858643ea
kmg2331 <115636546+kmg2331@users.noreply.github.com>
2022-11-30 02:36:12 +0900

    Update BoardDAO.java

--- src/main/java/com/springdream/app/repository/BoardDAO.java
+++ src/main/java/com/springdream/app/repository/BoardDAO.java
@@ -12,41 +12,19 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BoardDAO {
     private final BoardMapper boardMapper;
-<<<<<<< HEAD
-
-=======
->>>>>>> 96b6b6afb51683d395c065a2d524b8e3d8cb1165
     //    추가
     public void save(BoardVO boardVO){
         boardMapper.insert(boardVO);
     }
     //    수정
-<<<<<<< HEAD
-    public void setBoardVO(BoardVO boardVO){
-        boardMapper.update(boardVO);
-=======
     public void setBoardDTO(BoardDTO boardDTO){
         boardMapper.update(boardDTO);
->>>>>>> 96b6b6afb51683d395c065a2d524b8e3d8cb1165
     }
     //    삭제
     public void remove(Long boardNumber){
         boardMapper.delete(boardNumber);
     }
     //    조회
-<<<<<<< HEAD
-    public BoardVO findById(Long boardNumber){
-        return boardMapper.select(boardNumber);
-    }
-    //    전체 조회
-    public List<BoardVO> findAll(){
-        return boardMapper.selectAll();
-    }
-    //    전체 개수
-    public int findCountAll(){
-        return boardMapper.getTotal();
-    }
-=======
     public BoardDTO findByBoard(Long boardNumber){
         return boardMapper.select(boardNumber);
     }
@@ -59,5 +37,4 @@ public class BoardDAO {
 //    public int findCountAll(){
 //        return boardMapper.getTotal();
 //    }
->>>>>>> 96b6b6afb51683d395c065a2d524b8e3d8cb1165
 }

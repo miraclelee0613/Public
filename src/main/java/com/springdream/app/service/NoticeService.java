e7ffe52e4e7dae787586ea68271f37a1de8ee76b
leejunsang <miraclelee0613@gmail.com>
2022-12-04 18:52:45 +0900

    Update notice-all.html notice-recent.html add NoticeController NoticeService

---
+++ src/main/java/com/springdream/app/service/NoticeService.java
@@ -0,0 +1,11 @@
+package com.springdream.app.service;
+
+import com.springdream.app.domain.BoardDTO;
+import com.springdream.app.domain.BoardVO;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NoticeService{
+
+
+}
